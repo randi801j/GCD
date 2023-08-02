a191a199e515d3af61d6a6e8265d52e6a98eddf5
Randy Luc <113475637+randi801j@users.noreply.github.com>
2023-08-01 18:56:46 -0700

    Update GCF.js

--- Greatest Common Divisor of String/GCF.js
+++ Greatest Common Divisor of String/GCF.js
@@ -4,9 +4,9 @@ var gcfofString= function (str1,str2){
     let m= str1.length
     let n= str2.length
     let gcd = function (m,n){
-        if (!n) return m // if length equal to 0 return x
-        return gcd(n,m%n)
+        if (!n) return m // if length equal to 0 return m. Recursive, it checks if n is equal to 0
+        return gcd(n,m%n)// Recursive step, calling itself with argument n and m%n. % operator computes te remainder when m is divided by n. 
     }
-    let div = gcd(m,n)
+    let div = gcd(m,n) // this calls the gcd lengths m and n of str1 and str2 and stores the result in the cariable div. 
     return str1.slice (0,div) // slice from 0 index to the last of the second strong
 };
